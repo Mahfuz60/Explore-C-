f0ea1b149864a1a123a46ef5236e12dead06ab51
mahfuz60 <mahfuzuralam1996@gmail.com>
2022-05-25 03:03:50 +0600

    two number sum code

--- sum.cpp
+++ sum.cpp
@@ -1,5 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
+  int a;
+  cin >> a;
   
+  int b;
+  cin >> b;
+
+  int sum = a + b;
+
+  cout << "total sum is:" <<sum << endl;
+  return 0;
 }
