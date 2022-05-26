3d350701f44527226b70f89f355c6d0f731ab342
mahfuz60 <mahfuzuralam1996@gmail.com>
2022-05-27 01:38:51 +0600

    switch-case code

---
+++ switchCase.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  char button;
+  cout << "enter your input:";
+  cin >> button;
+
+  /*if (button == 'a')
+  {
+    cout << "Hello Sir" << endl;
+  }
+  else if (button == 'b')
+  {
+    cout << "Aslamualaikum sir" << endl;
+  }
+  else if (button == 'c')
+  {
+    cout << "Good Morning sir" << endl;
+  }
+  else if (button == 'd')
+  {
+    cout << "Good afternoon sir" << endl;
+  }
+  else if (button == 'e')
+  {
+    cout << "Good Night Sir" << endl;
+  }
+  else
+  {
+    cout << "I am still Learing now!" << endl;
+  }
+  */
+
+  switch (button)
+  {
+  case 'a':
+  {
+    cout << "Hello sir";
+    break;
+  }
+  case 'b':
+  {
+    cout << "Hello Mam";
+    break;
+  }
+  case 'c':
+  {
+    cout << "Hello Friends";
+    break;
+  }
+  case 'd':
+  {
+    cout << "Hi girls";
+    break;
+  }
+  case 'e':
+  {
+    cout << "hi boys";
+    break;
+  }
+
+  default:
+  {
+    cout << "Still try to good understading";
+    break;
+  }
+  }
+
+  return 0;
+}
